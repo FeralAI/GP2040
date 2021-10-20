bf265c4cec9c28672d7fe438b1c73f744d9aa78b
FeralAI <13342258+FeralAI@users.noreply.github.com>
2021-10-19 23:48:40 -0500

    Add React build script and conversion tools

---
+++ build-web.py
@@ -0,0 +1,23 @@
+import os
+import os.path
+
+website_dir = "www/build/"
+fsdata_filename = "lib/httpd/fsdata.c"
+
+print("Building React app")
+os.chdir("www")
+if not os.path.isdir("node_modules"):
+  print("Running npm install")
+  os.system("npm i")
+print("Running npm run build")
+os.system("npm run build")
+os.chdir("..")
+print("Done")
+
+print("Regenerating " + fsdata_filename)
+os.system("./tools/makefsdata " + website_dir + " -f:" + fsdata_filename)
+print("Done")
+
+print("Replace includes")
+os.system("perl -i -p0e 's/#include.*?\"lwip\/def.h\"/#include \"fsdata.h\"/s' " + fsdata_filename)
+print("Done")
