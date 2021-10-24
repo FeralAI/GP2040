bbd131e9ba28a9a805ddb48e4c6dedb861458664
FeralAI <13342258+FeralAI@users.noreply.github.com>
2021-10-24 14:24:02 -0500

    Update web app

--- www/src/Pages/PinMapping.js
+++ www/src/Pages/PinMapping.js
@@ -8,24 +8,24 @@ import boardParams from '../Data/BoardParams.json'
 import './PinMappings.scss';
 
 const baseButtonMappings = {
-	Up:    { pin: -1, xinput: 'Up', switch: 'Up', ps3: 'Up', dinput: 'Up', arcade: 'Up', error: null },
-	Down:  { pin: -1, xinput: 'Down', switch: 'Down', ps3: 'Down', dinput: 'Down', arcade: 'Down', error: null },
-	Left:  { pin: -1, xinput: 'Left', switch: 'Left', ps3: 'Left', dinput: 'Left', arcade: 'Left', error: null },
-	Right: { pin: -1, xinput: 'Right', switch: 'Right', ps3: 'Right', dinput: 'Right', arcade: 'Right', error: null },
-	B1:    { pin: -1, xinput: 'A', switch: 'B', ps3: 'Cross', dinput: '2', arcade: 'K1', error: null },
-	B2:    { pin: -1, xinput: 'B', switch: 'A', ps3: 'Circle', dinput: '3', arcade: 'K2', error: null },
-	B3:    { pin: -1, xinput: 'X', switch: 'Y', ps3: 'Square', dinput: '1', arcade: 'P1', error: null },
-	B4:    { pin: -1, xinput: 'Y', switch: 'X', ps3: 'Triangle', dinput: '4', arcade: 'P2', error: null },
-	L1:    { pin: -1, xinput: 'LB', switch: 'L', ps3: 'L1', dinput: '5', arcade: 'L1', error: null },
-	R1:    { pin: -1, xinput: 'RB', switch: 'R', ps3: 'R1', dinput: '6', arcade: 'R1', error: null },
-	L2:    { pin: -1, xinput: 'LT', switch: 'ZL', ps3: 'L2', dinput: '7', arcade: 'L2', error: null },
-	R2:    { pin: -1, xinput: 'RT', switch: 'ZR', ps3: 'R2', dinput: '8', arcade: 'R2', error: null },
-	S1:    { pin: -1, xinput: 'Back', switch: 'Minus', ps3: 'Select', dinput: '9', arcade: 'Coin', error: null },
-	S2:    { pin: -1, xinput: 'Start', switch: 'Plus', ps3: 'Start', dinput: '10', arcade: 'Start', error: null },
-	L3:    { pin: -1, xinput: 'LS', switch: 'LS', ps3: 'L3', dinput: '11', arcade: 'L3', error: null },
-	R3:    { pin: -1, xinput: 'RS', switch: 'RS', ps3: 'R3', dinput: '12', arcade: 'R3', error: null },
-	A1:    { pin: -1, xinput: 'Guide', switch: 'Home', ps3: 'N/A', dinput: '13', arcade: 'Home', error: null },
-	A2:    { pin: -1, xinput: 'N/A', switch: 'Capture', ps3: 'N/A', dinput: '14', arcade: 'N/A', error: null },
+	Up:    { pin: -1, button: 'Up', xinput: 'Up', switch: 'Up', ps3: 'Up', dinput: 'Up', arcade: 'Up', error: null },
+	Down:  { pin: -1, button: 'Down', xinput: 'Down', switch: 'Down', ps3: 'Down', dinput: 'Down', arcade: 'Down', error: null },
+	Left:  { pin: -1, button: 'Left', xinput: 'Left', switch: 'Left', ps3: 'Left', dinput: 'Left', arcade: 'Left', error: null },
+	Right: { pin: -1, button: 'Right', xinput: 'Right', switch: 'Right', ps3: 'Right', dinput: 'Right', arcade: 'Right', error: null },
+	B1:    { pin: -1, button: 'B1', xinput: 'A', switch: 'B', ps3: 'Cross', dinput: '2', arcade: 'K1', error: null },
+	B2:    { pin: -1, button: 'B2', xinput: 'B', switch: 'A', ps3: 'Circle', dinput: '3', arcade: 'K2', error: null },
+	B3:    { pin: -1, button: 'B3', xinput: 'X', switch: 'Y', ps3: 'Square', dinput: '1', arcade: 'P1', error: null },
+	B4:    { pin: -1, button: 'B4', xinput: 'Y', switch: 'X', ps3: 'Triangle', dinput: '4', arcade: 'P2', error: null },
+	L1:    { pin: -1, button: 'L1', xinput: 'LB', switch: 'L', ps3: 'L1', dinput: '5', arcade: 'L1', error: null },
+	R1:    { pin: -1, button: 'R1', xinput: 'RB', switch: 'R', ps3: 'R1', dinput: '6', arcade: 'R1', error: null },
+	L2:    { pin: -1, button: 'L2', xinput: 'LT', switch: 'ZL', ps3: 'L2', dinput: '7', arcade: 'L2', error: null },
+	R2:    { pin: -1, button: 'R2', xinput: 'RT', switch: 'ZR', ps3: 'R2', dinput: '8', arcade: 'R2', error: null },
+	S1:    { pin: -1, button: 'S1', xinput: 'Back', switch: 'Minus', ps3: 'Select', dinput: '9', arcade: 'Coin', error: null },
+	S2:    { pin: -1, button: 'S2', xinput: 'Start', switch: 'Plus', ps3: 'Start', dinput: '10', arcade: 'Start', error: null },
+	L3:    { pin: -1, button: 'L3', xinput: 'LS', switch: 'LS', ps3: 'L3', dinput: '11', arcade: 'L3', error: null },
+	R3:    { pin: -1, button: 'R3', xinput: 'RS', switch: 'RS', ps3: 'R3', dinput: '12', arcade: 'R3', error: null },
+	A1:    { pin: -1, button: 'A1', xinput: 'Guide', switch: 'Home', ps3: 'N/A', dinput: '13', arcade: 'Home', error: null },
+	A2:    { pin: -1, button: 'A2', xinput: 'N/A', switch: 'Capture', ps3: 'N/A', dinput: '14', arcade: 'N/A', error: null },
 };
 
 const buttonLabelOptions = [
@@ -75,69 +75,70 @@ export default function PinMappingPage() {
 		setSelectedButtonLabels(buttonLabelOptions.filter(o => o.value === e.target.value)[0]);
 	};
 
-	const handlePinChange = (e, i) => {
+	const handlePinChange = (e, prop) => {
 		const newMappings = {...buttonMappings};
 		if (e.target.value)
-			newMappings[i].pin = parseInt(e.target.value);
+			newMappings[prop].pin = parseInt(e.target.value);
 		else
-			newMappings[i].pin = '';
+			newMappings[prop].pin = '';
 
 		validateMappings(newMappings);
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		e.stopPropagation();
 
 		let newMappings = {...buttonMappings};
 		validateMappings(newMappings);
 
-		const form = e.currentTarget;
-		if (form.checkValidity() === false) {
+		if (Object.keys(newMappings).filter(p => !!newMappings[p].error).length) {
 			console.error('Invalid!');
+			return;
+		}
+
+		const data = Object.keys(newMappings).reduce((last, next) => {
+			let prefix = '';
+			if (last)
+				prefix = `${last}|`;
+
+			return `${prefix}${next}:${newMappings[next].pin}`;
+		}, '');
+
+		if (process.env.NODE_ENV === 'production') {
+			axios.get(`cgi/action?method=setPinMappings&mappings=${data}`)
+				.then((response) => {
+					console.log(response.data);
+					// TODO: Show success message
+				})
+				.catch(console.error);
 		}
 		else {
-			const data = newMappings.reduce((last, next) => {
-				let prefix = '';
-				if (last)
-					prefix = `${last}|`;
-
-				return `${prefix}${next.button}:${next.pin}`;
-			}, '');
-
-			if (process.env.NODE_ENV === 'production') {
-				axios.get(`cgi/action?method=setPinMappings&mappings=${data}`)
-					.then((response) => {
-						console.log(response.data);
-						// TODO: Show success message
-					})
-					.catch(console.error);
-			}
-			else {
-				console.log(data);
-			}
+			console.log(data);
 		}
-
-		setValidated(true);
 	};
 
 	const validateMappings = (mappings) => {
-		for (let mapping of mappings) {
-			mapping.error = null;
-			for (let otherMapping of mappings) {
-				if (mapping.error || mapping.button === otherMapping.button)
+		const props = Object.keys(mappings);
+
+		for (let prop of props) {
+			mappings[prop].error = null;
+
+			for (let otherProp of props) {
+				if (prop === otherProp)
 					continue;
 
-				if (mapping.pin === '' && !requiredButtons.filter(b => b === otherMapping.button).length)
-					mapping.error = `${mapping.button} is required`;
-				else if (mapping.pin === otherMapping.pin)
-					mapping.error = `Pin ${mapping.pin} is already assigned`;
-				else if (boardParams[selectedBoard]?.invalid.filter(p => p === mapping.pin).length > 0)
-					mapping.error = `Pin ${mapping.pin} is invalid for this board`;
+				if (mappings[prop].pin === '' && !requiredButtons.filter(b => b === mappings[otherProp].button).length)
+					mappings[prop].error = `${mappings[prop].button} is required`;
+				else if (mappings[prop].pin === mappings[otherProp].pin)
+					mappings[prop].error = `Pin ${mappings[prop].pin} is already assigned`;
+				else if (boardParams[selectedBoard]?.invalid.filter(p => p === mappings[prop].pin).length > 0)
+					mappings[prop].error = `Pin ${mappings[prop].pin} is invalid for this board`;
 			}
+
 		}
 
 		setButtonMappings(mappings);
+		setValidated(true);
 	};
 
 	return (
@@ -174,7 +175,7 @@ export default function PinMappingPage() {
 										max={boardParams[selectedBoard]?.max}
 										isInvalid={!!buttonMappings[prop].error}
 										required={requiredButtons.filter(b => b === buttonMappings[prop].button).length}
-										onChange={(e) => handlePinChange(e, i)}
+										onChange={(e) => handlePinChange(e, prop)}
 									></Form.Control>
 									<Form.Control.Feedback type="invalid">{buttonMappings[prop].error}</Form.Control.Feedback>
 								</td>
