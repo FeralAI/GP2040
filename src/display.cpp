10ab4b988443323a80aa8e25df71ba380f8910c4
FeralAI <13342258+FeralAI@users.noreply.github.com>
2021-11-25 22:12:07 -0600

    Add file header

--- src/display.cpp
+++ src/display.cpp
@@ -1,3 +1,8 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2021 Jason Skuby (mytechtoybox.com)
+ */
+
 #include <string>
 #include "display.h"
 #include "pico/stdlib.h"
