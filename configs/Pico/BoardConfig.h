33ea4c42350eefbd760ae31be95cf711500af7cb
Dylan Turner <dylantdmt@gmail.com>
2022-08-28 18:02:50 -0500

    Switched pins to be correct for fightkey

--- configs/Pico/BoardConfig.h
+++ configs/Pico/BoardConfig.h
@@ -10,18 +10,20 @@
 
 #define PIN_DPAD_UP     2
 #define PIN_DPAD_DOWN   3
-#define PIN_DPAD_RIGHT  4
-#define PIN_DPAD_LEFT   5
-#define PIN_BUTTON_B1   6
-#define PIN_BUTTON_B2   7
-#define PIN_BUTTON_R2   8
-#define PIN_BUTTON_L2   9
-#define PIN_BUTTON_B3   10
-#define PIN_BUTTON_B4   11
-#define PIN_BUTTON_R1   12
-#define PIN_BUTTON_L1   13
-#define PIN_BUTTON_S1   16
-#define PIN_BUTTON_S2   17
+#define PIN_DPAD_RIGHT  5
+#define PIN_DPAD_LEFT   4
+#define PIN_BUTTON_B1   9
+#define PIN_BUTTON_B2   10
+#define PIN_BUTTON_R2   15
+#define PIN_BUTTON_L2   12
+#define PIN_BUTTON_B3   13
+#define PIN_BUTTON_B4   17
+#define PIN_BUTTON_R1   11
+#define PIN_BUTTON_L1   16
+#define PIN_BUTTON_S1   8
+#define PIN_BUTTON_S2   7
+
+// Not connected on mine
 #define PIN_BUTTON_L3   18
 #define PIN_BUTTON_R3   19
 #define PIN_BUTTON_A1   20
